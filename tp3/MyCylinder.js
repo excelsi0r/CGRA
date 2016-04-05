3be62d0681898eee4f794fbf8a59f6d457b8110f
excelsi0r <nuno_mgl@hotmail.com>
2016-04-05 23:37:24 +0100

    improved

--- tp3/MyCylinder.js
+++ tp3/MyCylinder.js
@@ -48,13 +48,10 @@
 	var y = 0;
 	var z = 0;
 
-	var H = 1/this.stacks;	//HEIGHT
-	var R = 1;	//RADIX
+	var H = 1 / this.stacks;	//HEIGHT
+	var R = 1;	//RADIUS
 
 	var i0 = 0;  //INDDEX
-	var i1 = 1;
-	var i2 = 2;
-	var i3 = 3;
 
 	var nx = 0;	//NORMALS
 	var ny = 0;
@@ -69,96 +66,60 @@
 
 	for(j = 0; j < this.stacks; j++)
 	{
+
 		if(j == 0)
 		{
-		//First Normals
-		nx = Math.cos(alpha);
-		ny = Math.sin(alpha);
+			for(i = 0; i < this.slices; i++)
+			{
+				x = R*Math.cos(alpha);
+				y = R*Math.sin(alpha);
 
-		x = R*Math.cos(alpha);
-		y = R*Math.sin(alpha);
+				nx = Math.cos(alpha);
+				ny = Math.sin(alpha);
 
-		this.vertices.push(x,y,z);
-		this.vertices.push(x,y,z+H);
-
-		this.normals.push(nx, ny, nz);
-		this.normals.push(nx, ny, nz);
-
-
-		for(i = 0; i < this.slices-1; i++)
-		{
-	
-			alpha += n;
-					
-			x = R*Math.cos(alpha);
-			y = R*Math.sin(alpha);
-
-			nx = Math.cos(alpha);
-			ny = Math.sin(alpha);
-			
-			this.vertices.push(x,y,z);
-			this.vertices.push(x,y,z+H);
-
-			this.normals.push(nx, ny, nz);
-			this.normals.push(nx, ny, nz);
-
-			this.indices.push(i2,i1,i0);
-			//console.log(i2,i1,i0);
-			this.indices.push(i1,i2,i3);
-			//console.log(i1,i2,i3);
-
-			i0 = i0 + 2;
-			i1 = i1 + 2;
-			i2 = i2 + 2;
-			i3 = i3 + 2;		
-		}
-
-
-		this.indices.push(0,i1,i0);
-		this.indices.push(i1,0,1);
-	
-		z += H;
+				this.vertices.push(x,y,z);
+				this.normals.push(nx, ny, nz);	
+				alpha += n;
+			}
+			z += H;
 		
 		}
 		else
 		{
-			nx = Math.cos(alpha);
-			ny = Math.sin(alpha);
-
-			x = R*Math.cos(alpha);
-			y = R*Math.sin(alpha);
-
-			this.vertices.push(x,y,z+H);
-			this.normals.push(nx, ny, nz);
-
-			i0 = i3 - 16;
-			i1 = i1 + 1;
-			i2 = i3 - 14;
-			i3 = i3;
-			
 			for(i = 0; i < this.slices-1; i++)
 			{
-				alpha += n;
-
 				nx = Math.cos(alpha);
 				ny = Math.sin(alpha);
 
 				x = R*Math.cos(alpha);
 				y = R*Math.sin(alpha);
 
-				this.vertices.push(x,y,z+H);
+				this.vertices.push(x,y,z);
 				this.normals.push(nx, ny, nz);
 
-				this.indices.push(i2,i1,i0);
-				this.indices.push(i1,i2,i3);
+				this.indices.push(i0+1,i0+this.slices,i0);
+				this.indices.push(i0+this.slices,i0+1,i0+this.slices+1);
+				i0++;
 
-				i0 = i0 + 1;
-				i1 = i1 + 1;
-				i2 = i2 + 1;
-				i3 = i3 + 1;	
+				alpha += n;
 			}
 
+			nx = Math.cos(alpha);
+			ny = Math.sin(alpha);
+
+			x = R*Math.cos(alpha);
+			y = R*Math.sin(alpha);
+
+			this.vertices.push(x,y,z);
+			this.normals.push(nx, ny, nz);
+
+			this.indices.push(i0,i0-(this.slices-1),i0+this.slices);
+			this.indices.push(i0-(this.slices-1),i0+1,i0+this.slices);	
+
+			i0 = j*this.slices - this.slices;
 			z += H;
+
+			alpha += n;
 		}
 
 	}
